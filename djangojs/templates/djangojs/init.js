bd0f7b56d8ab2ae77795797fd10812d0b76883dc
Eugene Kim <p.compassion@gmail.com>
2015-02-17 21:23:33 +0900

    fix

--- djangojs/templates/djangojs/init.js
+++ djangojs/templates/djangojs/init.js
@@ -1,10 +1,13 @@
 (function(){
-	if (typeof is_server !== "undefined") {
-		console.log('init.js is_server');
-		window.DJANGO_JS_CSRF = false;
-	}
-	window.DJANGO_JS_INIT = true;
+    if (typeof is_server !== undefined) {
+        window.DJANGO_JS_CSRF = false;
+    }
+    else {
+        window.DJANGO_JS_CSRF = true;
+    }
 
-	window.DJANGO_JS_URLS = {{ urls|safe }};
-	window.DJANGO_JS_CONTEXT = {{ context|safe }};
+    window.DJANGO_JS_URLS = {{ urls|safe }};
+    window.DJANGO_JS_CONTEXT = {{ context|safe }};
+
+    window.DJANGO_JS_INIT = true;
 }());
